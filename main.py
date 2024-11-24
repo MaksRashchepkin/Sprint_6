2aea7c8be79ee595e2b0d0a94b7d9b83c8ebb181
Maks Rashchepkin <m.rashepkin2018@gmail.com>
2024-11-24 23:04:05 +0300

    Sprint_6

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-# This is a sample Python script.
+from selenium import webdriver
+import time
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# инициализируем драйвер браузера
+driver = webdriver.Chrome()
 
+# сделаем паузу
+time.sleep(3)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# закроем браузер
+driver.quit()
