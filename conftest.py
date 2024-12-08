01da3ef4acd376bd920d6ffa5fd91284f6e7a376
Maks Rashchepkin <m.rashepkin2018@gmail.com>
2024-12-08 23:06:10 +0300

    Sprint_6

--- conftest.py
+++ conftest.py
@@ -2,13 +2,11 @@ from selenium import webdriver
 import pytest
 from data import Url
 from locators.locators_main_page import MainPageLocators
-import time
 
 @pytest.fixture
 def driver():
     driver = webdriver.Firefox()
     driver.maximize_window()
-    time.sleep(1)
     yield driver
     driver.quit()
 
@@ -16,5 +14,4 @@ def driver():
 def cookies(driver):
     driver.get(Url.scooter_main_page)
     driver.find_element(*MainPageLocators.cookies_button).click()
-    time.sleep(1)
-    return cookies
+    return driver
